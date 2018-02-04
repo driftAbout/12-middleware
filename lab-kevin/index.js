94c8599dd72b34eff97790f371dc113d09a80f61
Kevin Miller <kevin@driftabout.com>
2018-02-04 12:35:11 -0800

    cleaned up the index.js

--- lab-kevin/index.js
+++ lab-kevin/index.js
@@ -2,6 +2,4 @@
 'use strict';
 const server = require('./lib/server');
 
-if(process.env.NODE_ENV !== 'production') require('dotenv').config();
-
-server.start(process.env.PORT, () => console.log(`Listening on PORT ${process.env.PORT}`));
+server.start();
